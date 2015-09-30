c919759dcecab7863573be4a886a3f01165c85d7
Ross <rdegnen@gmail.com>
2015-09-30 12:01:15 -0400

    removed boilerplate code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,2 @@
 /*global RouteTracker, $*/
 
-
-window.RouteTracker = {
-    Models: {},
-    Collections: {},
-    Views: {},
-    Routers: {},
-    init: function () {
-        'use strict';
-        console.log('Hello from Backbone!');
-    }
-};
-
-$(document).ready(function () {
-    'use strict';
-    RouteTracker.init();
-});
